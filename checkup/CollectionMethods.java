f54d03647e6cc59be206663b0be61d6215c16dd1
avergnaud <a.vergnaud@gmail.com>
2016-11-03 16:23:29 +0100

    revision

--- checkup/CollectionMethods.java
+++ checkup/CollectionMethods.java
@@ -1,1 +1,17 @@
+//interface java.util.Collection
+
+boolean add(E e)
+boolean addAll(Collection<? extends E> c)
+boolean remove(Object o)
+boolean removeAll(Collection<?> c)
+boolean contains(Object o)
+boolean isEmpty()
+void clear()
+int size()
+
+Object[] toArray()
+<T> T[] toArray(T[] a)
+
+default Stream<E> stream()
+default Stream<E> parallelStream()
 default boolean removeIf(Predicate<? super E> filter)
--- checkup/InstantMethods.java
+++ checkup/InstantMethods.java
@@ -1,16 +1,28 @@
+//class java.time.Instant implements Comparable<Instant>
+
+int compareTo(Instant otherInstant)
+
+static Instant now()
 
 static Instant ofEpochSecond(long)
-
-Instant plus(long, TemporalUnit unit) /*any unit day or smaller*/
-
-public long getEpochSecond()
+static Instant ofEpochMilli(long epochMilli)
+long toEpochMilli()
 
 static Instant parse(CharSequence text) /* 2007-12-03T10:15:30.00Z */
 
+public long getEpochSecond()
+
+boolean isAfter(Instant otherInstant)
+boolean isBefore(Instant otherInstant)
+
+Instant minus(long, TemporalUnit unit) /*any unit day or smaller*/
+Instant minus(TemporalAmount amountToSubstract) /* use Duration */
+Instant minusSeconds(long secondsToSubstract)
+Instant minusMillis(long millisToSubstract)
+Instant minusNanos(long nanosToSubstract)
+
+Instant plus(long, TemporalUnit unit) /*any unit day or smaller*/
 Instant plus(TemporalAmount amountToAdd) /* use Duration */
-
 Instant plusSeconds(long secondsToAdd)
-
 Instant plusMillis(long millisToAdd)
-
-Instant plusNanos(long nanosToAdd) 
+Instant plusNanos(long nanosToAdd)
